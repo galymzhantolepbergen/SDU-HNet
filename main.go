828eb489bf5e4f744fd8c6a460dc66dbea1ff405
Galymzhan Tolepbergen <galymjan2002g@mail.ru>
2023-04-10 14:18:53 +0600

    localhost 8080 changed

--- main.go
+++ main.go
@@ -1,23 +1,24 @@
 package main
 
 import (
-    "net/http"
+	"net/http"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    // Create a new Gin router
-    router := gin.Default()
+	// Create a new Gin router
+	router := gin.Default()
+	println("http://localhost:8080/")
 
-    // Switch to release mode
-    gin.SetMode(gin.ReleaseMode)
+	// Switch to release mode
+	gin.SetMode(gin.ReleaseMode)
 
-    // Define a route and a handler function
-    router.GET("/", func(c *gin.Context) {
-        c.String(http.StatusOK, "Hello, World!")
-    })
+	// Define a route and a handler function
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello, World!")
+	})
 
-    // Start the server
-    router.Run(":8080")
+	// Start the server
+	router.Run(":8080")
 }
